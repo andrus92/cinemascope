0579eb59fe9dee98a4f84158c7c131399e45f746
Andrei Yahorau <yahoraual92@gmail.com>
2021-11-07 16:18:18 +0300

    Created a function sortArrayByName in a separate module utils.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import "./styles.css";
+import { sortArrayByName } from './utils';
 
 document.getElementById("app").innerHTML = `<h1>Hello Cinemascope!</h1>`;
+
+const arr = [ "View", "React", "Angular" ];
+console.log(arr);
+const sortedArr = sortArrayByName(arr);
+console.log(sortedArr);
