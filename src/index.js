4dfec8af3d456f0bfd56c5c8c25beb9e08c76db1
Andrei Yahorau <yahoraual92@gmail.com>
2021-11-12 15:54:24 +0300

    Numerous imports of images were deleted

--- src/index.js
+++ src/index.js
@@ -3,24 +3,6 @@ import "./styles/styles.css";
 import { sortArrayByName } from './utils';
 import { moviesData } from './movies';
 
-import img0 from '../img/Shawshank_redemption_movie.jpg';
-import img1 from '../img/Godfather.jpg';
-import img2 from '../img/vita.jfif';
-import img4 from '../img/forest.jfif';
-import img5 from '../img/1plus1.jfif';
-import img6 from '../img/pulp_fiction.jfif';
-import img7 from '../img/a_beautiful_mind.jfif';
-import img8 from '../img/catch_me.jfif';
-import img9 from '../img/pirates.jfif';
-import img10 from '../img/scent_of_a_woman.jfif';
-import img11 from '../img/wonderful_life.jfif';
-import img12 from '../img/il_bisbetico_domato.jfif';
-import img13 from '../img/innamorato.jfif';
-import img14 from '../img/roman_holiday.jfif';
-import img15 from '../img/breakfast.jfif';
-import img16 from '../img/dial_m.jfif';
-import img17 from '../img/rear_window.jfif';
-import img18 from '../img/vertigo.jfif';
 
 function getRoot() {
     return document.getElementById("root");
@@ -44,10 +26,9 @@ function renderMovies() {
         let div = document.createElement("div");
         div.classList.add("movie__box");
 
-        const filmImg = require(film.picture);
         div.innerHTML =
             `<div class="movie__card">
-                <img src="${filmImg}" alt="" class="movie__img">
+                <img src="${film.picture}" alt="" class="movie__img">
                 <span class="movie__tittle">${film.title}</span>
                 <span class="movie__rating">${film.rating}</span>
                 <span class="movie__genres">${film.genres}</span>
