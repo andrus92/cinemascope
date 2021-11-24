cc59a598c9001eb340b23493ecf5f2c5e13f2362
Andrei Yahorau <yahoraual92@gmail.com>
2021-11-24 09:19:20 +0300

    Created an .env file for storing API_KEY

--- src/tmdbApi.js
+++ src/tmdbApi.js
@@ -1,13 +1,10 @@
-const API_KEY = '&api_key=c1136013db4178c15f6c05580bac0e31';
+const API_KEY = process.env.API_KEY; 
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-const API_URL = BASE_URL + 
-    '/discover/movie?with_genres=18&sort_by=vote_average.desc&vote_count.gte=20' +
-    API_KEY;
+const API_URL = `${BASE_URL}/discover/movie?with_genres=18&sort_by=vote_average.desc&vote_count.gte=20${API_KEY}`;
 
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-
 
 export const requestData = () => new Promise((resolve, reject) => {
     fetch(API_URL)
